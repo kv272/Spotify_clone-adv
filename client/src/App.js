34ab33aed4a125375d37c9e8547bfcfe732df1e7
kv272 <vermakunal177@gmail.com>
2024-05-12 20:46:15 +0530

    added final files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import Login from './component/Login';
-import Music from './component/Music';
-export default function App(){
-  return(
-  <div>
-    <Login/>
-    <Music/>
-  </div>
-  )
+import Sidebar from 'components/Sidebar'
+import BottomBar from 'components/BottomBar'
+import Content from 'components/Content'
+import { BrowserRouter as Router } from "react-router-dom"
+
+function App() {
+	return (
+		<Router>
+			<div className="wrapper">
+				<Sidebar/>
+				<Content/>
+			</div>
+			<BottomBar/>
+		</Router>
+	);
 }
+
+export default App;
